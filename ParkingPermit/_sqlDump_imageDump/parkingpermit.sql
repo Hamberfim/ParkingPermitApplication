14b78e477b182339a3f2d3b6325a9420bffbcf1d
Anthony Hamlin <adhamlingithub@gmail.com>
2020-04-21 16:29:47 -0500

    simplified db

--- ParkingPermit/_sqlDump_imageDump/parkingpermit.sql
+++ ParkingPermit/_sqlDump_imageDump/parkingpermit.sql
@@ -9,7 +9,7 @@ CREATE TABLE `studentparking` (
   `color` varchar(100) DEFAULT NULL,
   `platenumber` varchar(6) DEFAULT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1;
 
 
 INSERT INTO `studentparking` (firstname, lastname, make, model, color, platenumber) VALUES 
